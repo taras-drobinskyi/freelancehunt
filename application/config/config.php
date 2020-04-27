e61afbb2228daaa70d222f4a2a6c55c6dd79c727
t <t@devurai.com>
2020-04-27 12:18:43 +0300

    Clear config db and token parameters.

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'freelancehunt');
 define('DB_USER', 'root');
-define('DB_PASS', '123456');
+define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
 
-define('FREELANCE_API_TOKEN', 'bf3359c13e3996a679d6cf07fb3b7b57f0c991b7');
+define('FREELANCE_API_TOKEN', '');
